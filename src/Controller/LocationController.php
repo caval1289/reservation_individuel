ac43dce289b37fa9db651bf2931fa91000a08047
cavaleri <caval1289@gmail.com>
2023-04-17 18:41:23 +0200

    Controller Location + templates + routes

---
+++ src/Controller/LocationController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Location;
+use App\Repository\LocationRepository;
+
+#[Route('/location')]
+class LocationController extends AbstractController
+{
+    #[Route('/', name: 'location_index', methods: ['GET'])]
+    public function index(LocationRepository $repository): Response
+    {
+        $locations = $repository->findAll();
+
+        return $this->render('location/index.html.twig', [
+            'locations' => $locations,
+            'resource' => 'lieux',
+        ]);
+
+    }
+    #[Route('/location/{id}', name:'location_show', methods: ['GET'])]
+    public function show(int $id, LocationRepository $repository): Response
+    {
+        $location = $repository->find($id);
+
+        return $this->render('location/show.html.twig', [
+            'location' => $location,
+        ]);
+    }
+}
