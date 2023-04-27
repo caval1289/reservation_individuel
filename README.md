0d497b34aefc42e436306bdfed20fc43fe352d95
cavaleri <caval1289@gmail.com>
2023-04-27 19:55:16 +0200

    Form Artist

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
-Install Composer
+#Install Composer
 
-Install Symfony composer create-project symfony/skeleton nom_du_projet 5.4.* cd nom_du_projet composer require webapp
+#Install Symfony 
+composer create-project symfony/skeleton nom_du_projet 5.4.* cd nom_du_projet composer require webapp
 
-Install Project composer install
+#Install Project 
+composer install
 
-Start in local php -S 127.0.0.1:8000 -t public
+#Start in local 
 
-p85 77.	Répétez ces opérations pour les relations entre les modèles User et Role.
+php -S 127.0.0.1:8000 -t public
+
+p19 roadmap symfony6
--- src/Controller/ArtistController.php
+++ src/Controller/ArtistController.php
@@ -3,15 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Artist;
 use App\Repository\ArtistRepository;
+use App\Form\ArtistType;
 
 #[Route('/artist')]
 class ArtistController extends AbstractController
 {
-    #[Route('/', name:'artist_index', methods: ['GET'])]
+    #[Route('/', name: 'artist_index', methods: ['GET'])]
     public function index(ArtistRepository $repository): Response
     {
         $artists = $repository->findAll();
@@ -21,7 +23,7 @@ class ArtistController extends AbstractController
             'resource' => 'artistes',
         ]);
     }
-    #[Route('/{id}', name:'artist_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'artist_show', methods: ['GET'])]
     public function show(int $id, ArtistRepository $repository): Response
     {
         $artist = $repository->find($id);
@@ -30,8 +32,21 @@ class ArtistController extends AbstractController
             'artist' => $artist,
         ]);
     }
- 
+    #[Route('/{id}/edit', name: 'artist_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Artist $artist, ArtistRepository $artistRepository): Response
+    {
+        $form = $this->createForm(ArtistType::class, $artist);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $artistRepository->save($artist, true);
+
+            return $this->redirectToRoute('artist_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('artist/edit.html.twig', [
+            'artist' => $artist,
+            'form' => $form->createView(),
+        ]);
+    }
 }
-
-
-
